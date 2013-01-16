d47fac03d3b731369512fb3a5b0100aa11ba6454
Iskander Haziev <gvalmon@gmail.com>
2013-01-16 07:26:00 +0400

    fix mongo config for heroku

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -5,13 +5,18 @@ if ENV['MONGOHQ_URL']
   db_name = db.path.gsub(/^\//, '')
   db_host = db.host
   db_port = db.port
+
+  connection = Mongo::Connection.new(db_host, db_port)
+  connection.db(db_name).authenticate(db.user, db.password)
 else
   db_name = "webmate"
   db_host = "localhost"
   db_port = "27017"
+
+  connection = Mongo::Connection.new(db_host, db_port)
 end
 
 Mongoid.configure do |config|
   config.allow_dynamic_fields = true
-  config.master = Mongo::Connection.new(db_host, db_port).db(db_name)
+  config.master = connection.db(db_name)
 end
