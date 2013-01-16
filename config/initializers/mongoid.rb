6ba2e8a9a7fa48a1e66a243583856a96b8fc1ca1
Iskander Haziev <gvalmon@gmail.com>
2013-01-16 05:54:14 +0400

    added mongo config for heroku

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,8 +1,17 @@
 require 'mongoid'
-# Mongoid.load!('./config/mongoid.yml', :development)
+
+if ENV['MONGOHQ_URL']
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  db_host = db.host
+  db_port = db.port
+else
+  db_name = "webmate"
+  db_host = "localhost"
+  db_port = "27017"
+end
+
 Mongoid.configure do |config|
-   name = "webmate"
-   host = "localhost"
-   config.allow_dynamic_fields = true
-   config.master = Mongo::Connection.new.db(name)
+  config.allow_dynamic_fields = true
+  config.master = Mongo::Connection.new(db_host, db_port).db(db_name)
 end
