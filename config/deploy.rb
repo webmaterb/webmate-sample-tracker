501353700eba9b9a09c5516c7f5a9945525c93c0
Iskander Haziev <gvalmon@gmail.com>
2013-01-17 16:34:26 +0400

    update capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,19 @@ require 'bundler/capistrano'
 require 'capistrano-rbenv'
 
 set :stages, %w(staging production)
-set :default_stage, "staging"
+set :default_stage, "production"
 set :deploy_via, :copy
 set :keep_releases, 5
 set :scm, :git
+set :server_type, "thin"
 
-before  'deploy:assets:precompile', 'db:create_symlink'
-after   'deploy:create_symlink', 'deploy:cleanup'
+after     'deploy:update_code', 'thin:create_symlink'
+after     'deploy:update_code', 'db:create_symlink'
+after     'deploy:create_symlink', 'deploy:cleanup'
+
+namespace :db do
+  desc "Make symlink for mongoid.yml"
+  task :create_symlink do
+    run "ln -nfs #{shared_path}/config/mongoid.yml #{latest_release}/config/mongoid.yml"
+  end
+end
