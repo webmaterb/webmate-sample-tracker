c6dd7b996171df70de7cfbcb3fa78b4eb99105c3
Iskander Haziev <gvalmon@gmail.com>
2013-01-19 21:41:22 +0400

    upload attachment triggers task update

--- app/responders/attachments_responder.rb
+++ app/responders/attachments_responder.rb
@@ -1,14 +1,12 @@
 class AttachmentsResponder < BaseResponder
   def create
-    puts params[:attachment][:task_id]
     task = Task.find(params[:attachment][:task_id])
     params[:files].each do |file|
       attachment = Attachment.new
       attachment.file = file[:tempfile]
       task.attachments << attachment
     end
-    task.save!
-    puts task.inspect
-    task
+    task.save
+    respond_with(task, action: 'tasks/update')
   end
 end
